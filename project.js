0ced0674342c3f4d6f404c338ccde5ecbdd65d87
Jason Butler <jjbutler74@gmail.com>
2016-06-01 21:43:12 -0400

    Copywrite and animations

--- project.js
+++ project.js
@@ -55,37 +55,31 @@ $(document).ready(function() {
 			if (value.name == projectName)
 			{
 				detailHtml = detailHtml + '<div class="project-details">';
-
-				
+				detailHtml = detailHtml + '<div class="wow fadeInDown">'
 				detailHtml = detailHtml + '<h1>' + value.title + '</h1>';
 				detailHtml = detailHtml + '<h3 class="hidden-xs">' + value.description + '</h3>';
-				
 				detailHtml = detailHtml + '<div class="line-thin"></div>';
-				
 				detailHtml = detailHtml + '<h4 class="hidden-xs">' + value.caption + '</h4>';
-				
+				detailHtml = detailHtml + '</div>';
 				
 				$.each( value.screens, function( key2, value2 ) {
 					detailHtml = detailHtml + '<div class="row margin-top-20">';
 					
-					detailHtml = detailHtml + '<div class="col-md-9 col-md-push-3">'
+					detailHtml = detailHtml + '<div class="col-md-9 col-md-push-3 wow fadeInRight">'
 					detailHtml = detailHtml + '<img class="center-block img-responsive" src="' + value2.image + '">';
 					detailHtml = detailHtml + '</div>';
 					
-					detailHtml = detailHtml + '<div class="col-md-3 col-md-pull-9">'
+					detailHtml = detailHtml + '<div class="col-md-3 col-md-pull-9 wow fadeInLeft">'
 					detailHtml = detailHtml + '<p>' + value2.description + '</p>';
 					detailHtml = detailHtml + '</div>';
 					
 					detailHtml = detailHtml + '</div>';
 				});
-				
 				if (value.final != undefined)
 				{
-					detailHtml = detailHtml + '<p>' + value.final + '</p>';
+					detailHtml = detailHtml + '<p class="wow fadeInUp">' + value.final + '</p>';
 				}
-				
 				detailHtml = detailHtml + '</div>';
-				
 			}
 		});
 		return detailHtml;
